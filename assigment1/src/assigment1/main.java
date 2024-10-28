de7fc351d709ee6bd89ffcf1fe201a362ecb37d4
Thanhcao113 <thanhcao2362005@gmail.com>
2024-10-28 16:32:48 +0700

    first commit

---
+++ assigment1/src/assigment1/main.java
@@ -0,0 +1,16 @@
+package assigment1;
+public class main {
+    public static void main(String[] args) {
+        String hoVaTen = "Cao Tan Thanh";
+        String lop = "12C3";
+        String maSoSinhVien = "123456";
+
+        System.out.println("Họ và tên: " + hoVaTen);
+        System.out.println("Lớp: " + lop);
+        System.out.println("Mã số sinh viên: " + maSoSinhVien);
+    }
+}   
+	
+
+
+
